efd7712c39b04ed74074ce7247e505dedb3a5de9
StrongBreeze <130485186+StrongBreeze@users.noreply.github.com>
2023-04-18 16:32:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,51 @@ function main(){
 });
 };
 $(document).ready(main);
+
+var curIndex = 0;
+    imgDuration = 10000;
+    slider = document.getElementById("slider");
+    slides = slider.childNodes; //get a hook on all child elements, this is live so anything we add will get listed
+    imgArray = [
+        'bickleyhall_sb_hero.jpg',
+        'chobham_sb_hero.jpg'
+        ];
+    
+// Dynamically add each slideshow image frame into the DOM;
+function buildSlideShow(arr) {
+    for (i = 0; i < arr.length; i++) {
+        var img = document.createElement('img');
+        img.src = arr[i];
+        slider.appendChild(img);
+    }
+    // note the slides reference will now contain the images so we can access them
+}
+
+// Our slideshow function, we can call this and it flips the image instantly, once it is called it will roll
+// our images at given interval [imgDuration];
+
+function slideShow() {
+    
+    function fadeIn(e) {
+        e.className = "fadeIn";
+    };
+
+    function fadeOut(e) {
+        e.className = "";
+    };
+
+        fadeOut(slides[curIndex]);
+        curIndex++;
+        if (curIndex === slides.length) {
+            curIndex = 0;
+        }
+        
+        fadeIn(slides[curIndex]);
+
+        setTimeout(function () {
+            slideShow();
+        }, imgDuration);
+    };  //end of slideshow function
+
+    buildSlideShow(imgArray);
+    slideShow();
